3e3ed135cb619c6cb969e0428cae6ba2a7438e82
tseked <lineked@yandex.ru>
2026-01-19 21:39:54 +0300

    Rename UserEntity to GroupEntity in core:database

--- core/database/src/commonMain/kotlin/com/cyberstarege/kotfinance/model/GroupEntity.kt
+++ core/database/src/commonMain/kotlin/com/cyberstarege/kotfinance/model/GroupEntity.kt
@@ -3,8 +3,8 @@ package com.cyberstarege.kotfinance.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "users")
-data class UserEntity(
+@Entity(tableName = "groups")
+data class GroupEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val name : String
