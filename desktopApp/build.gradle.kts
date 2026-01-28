72d2a8a296964410fbd7242257ae468021695ca6
tseked <lineked@yandex.ru>
2026-01-28 10:14:17 +0300

    Add desktopApp module

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    dependencies {
+        implementation(projects.kotfinance.composeApp)
+
+        implementation(compose.desktop.currentOs)
+        implementation(libs.kotlinx.coroutinesSwing)
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.cyberstarege.kotfinance.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.cyberstarege.kotfinance"
+            packageVersion = "1.0.0"
+        }
+    }
+}
--- desktopApp/src/main/kotlin/com/cyberstarege/kotfinance/main.kt
+++ desktopApp/src/main/kotlin/com/cyberstarege/kotfinance/main.kt
@@ -1,7 +1,8 @@
-package com.cyberstarege.kotfinance
+package com.cyberstarege.kotfinance.com.cyberstarege.kotfinance
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.cyberstarege.kotfinance.App
 
 fun main() = application {
     Window(
