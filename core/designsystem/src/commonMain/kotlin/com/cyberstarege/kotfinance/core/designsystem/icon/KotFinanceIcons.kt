856da07f34528e64e256c1875085a39933406a4b
tseked <lineked@yandex.ru>
2026-01-28 17:51:37 +0300

    Add ic_add

--- core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/KotFinanceIcons.kt
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/KotFinanceIcons.kt
@@ -2,7 +2,8 @@ package com.cyberstarege.kotfinance.core.designsystem.icon
 
 import kotfinance.core.designsystem.generated.resources.Res
 import kotfinance.core.designsystem.generated.resources.ic_edit
-
+import kotfinance.core.designsystem.generated.resources.ic_add
 object KotFinanceIcons {
     val Edit = Res.drawable.ic_edit
+    val Add = Res.drawable.ic_add
 }
