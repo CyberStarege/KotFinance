0334541f017caf19d2d2725160646498923d7768
tseked <lineked@yandex.ru>
2026-01-28 17:49:56 +0300

    Add new fields to AccountGroup

--- core/model/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/model/AccountGroup.kt
+++ core/model/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/model/AccountGroup.kt
@@ -2,5 +2,8 @@ package com.cyberstarege.kotfinance.core.model
 
 data class AccountGroup(
     val id: Long = 0,
-    val name: String
+    val name: String,
+    val description: String? = null,
+    val color: Int,
+    val icon: String? = null
 )
