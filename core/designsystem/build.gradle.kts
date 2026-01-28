d2fb1d29c32c6e962f7104000c2a05ad943f639b
tseked <lineked@yandex.ru>
2026-01-28 10:11:38 +0300

    Add new module core:designsystem

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.multiplatformLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+
+kotlin {
+    jvm()
+
+    androidLibrary {
+        namespace = "com.cyberstarege.kotfinance.core.designsystem"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+
+        withHostTestBuilder {
+
+        }
+
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        localDependencySelection {
+            // For dependencies with multiple build types, select 'debug' first, and 'release' in case 'debug' is missing
+            selectBuildTypeFrom.set(listOf("debug", "release"))
+        }
+
+        androidResources {
+            enable = true
+        }
+
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.jetbrains.compose.runtime)
+            implementation(libs.jetbrains.compose.foundation)
+            implementation(libs.jetbrains.compose.material3)
+            implementation(libs.jetbrains.compose.ui)
+            implementation(libs.jetbrains.compose.components.resources)
+            implementation(libs.jetbrains.compose.ui.tooling.preview)
+        }
+
+        androidMain.dependencies {
+            implementation(libs.jetbrains.compose.ui.tooling.preview)
+        }
+    }
+}
+
+dependencies {
+    androidRuntimeClasspath(libs.jetbrains.compose.ui.tooling)
+}
+
