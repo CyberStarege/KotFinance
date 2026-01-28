41721c9e2c88185129b48e946b1d5b473579cb5a
tseked <lineked@yandex.ru>
2026-01-28 23:14:22 +0300

    Add initKoin

--- desktopApp/src/main/kotlin/com/cyberstarege/kotfinance/main.kt
+++ desktopApp/src/main/kotlin/com/cyberstarege/kotfinance/main.kt
@@ -3,8 +3,12 @@ package com.cyberstarege.kotfinance.com.cyberstarege.kotfinance
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.cyberstarege.kotfinance.App
+import com.cyberstarege.kotfinance.initKoin
 
 fun main() = application {
+    initKoin {
+
+    }
     Window(
         onCloseRequest = ::exitApplication,
         title = "kotfinance",
