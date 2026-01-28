248049c6548848ada930414a4677767e72c853fa
tseked <lineked@yandex.ru>
2026-01-28 17:50:03 +0300

    Add new fields to AccountGroupEntity

--- core/database/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/database/model/AccountGroupEntity.kt
+++ core/database/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/database/model/AccountGroupEntity.kt
@@ -7,5 +7,8 @@ import androidx.room.PrimaryKey
 data class AccountGroupEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    val name : String
+    val name : String,
+    val description: String? = null,
+    val color: Int,
+    val icon: String? = null
 )
