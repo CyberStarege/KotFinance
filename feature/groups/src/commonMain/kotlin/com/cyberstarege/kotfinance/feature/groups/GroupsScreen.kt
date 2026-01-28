fffccf51d869e670818a8f9aca9dbbe78236006f
tseked <lineked@yandex.ru>
2026-01-28 10:14:24 +0300

    add GroupsScreen

---
+++ feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsScreen.kt
@@ -0,0 +1,16 @@
+package com.cyberstarege.kotfinance.feature.groups
+
+import androidx.compose.runtime.Composable
+import com.cyberstarege.kotfinance.core.model.AccountGroup
+
+@Composable
+fun GroupsScreen() {
+
+}
+
+@Composable
+fun AccountGroupItem(
+    accountGroup: AccountGroup,
+) {
+
+}
