b01521a46f368b65b45f7d94066cb44a7816b7ef
tseked <lineked@yandex.ru>
2026-01-28 13:20:06 +0300

    add groupsViewModel

--- feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsScreen.kt
+++ feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsScreen.kt
@@ -2,10 +2,11 @@ package com.cyberstarege.kotfinance.feature.groups
 
 import androidx.compose.runtime.Composable
 import com.cyberstarege.kotfinance.core.model.AccountGroup
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun GroupsScreen() {
-
+    val groupsViewModel = koinViewModel<GroupsViewModel>()
 }
 
 @Composable
