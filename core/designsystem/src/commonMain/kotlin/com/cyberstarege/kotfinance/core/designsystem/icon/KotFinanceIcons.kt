43b4ed92064aa865b2a05b9a3bf87c591fcfb616
tseked <lineked@yandex.ru>
2026-01-28 13:21:38 +0300

    Add KotFinanceIcons object

---
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/KotFinanceIcons.kt
@@ -0,0 +1,8 @@
+package com.cyberstarege.kotfinance.core.designsystem.icon
+
+import kotfinance.core.designsystem.generated.resources.Res
+import kotfinance.core.designsystem.generated.resources.ic_edit
+
+object KotFinanceIcons {
+    val Edit = Res.drawable.ic_edit
+}
