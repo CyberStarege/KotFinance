ebc30e7bfd937d2e99e009153e6be6d7d6c6c8d4
tseked <lineked@yandex.ru>
2026-01-28 23:14:33 +0300

    Update GroupsViewModel

--- feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
+++ feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
@@ -1,10 +1,29 @@
 package com.cyberstarege.kotfinance.feature.groups
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cyberstarege.kotfinance.core.domain.AddGroupUseCase
 import com.cyberstarege.kotfinance.core.domain.GetAllAccountGroupsUseCase
+import com.cyberstarege.kotfinance.core.model.AccountGroup
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class GroupsViewModel(
-    private val getAllAccountGroupsUseCase: GetAllAccountGroupsUseCase
+    private val getAllAccountGroupsUseCase: GetAllAccountGroupsUseCase,
+    private val addGroupUseCase: AddGroupUseCase
 ) : ViewModel() {
+    val accountGroups: StateFlow<List<AccountGroup>> = getAllAccountGroupsUseCase()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = emptyList()
+        )
 
+    fun addGroup(accountGroup: AccountGroup) {
+        viewModelScope.launch {
+            addGroupUseCase(accountGroup)
+        }
+    }
 }
