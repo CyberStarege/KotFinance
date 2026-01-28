fb223fdc6f4d5deeb50ee02a145a7ebaf1bdca24
tseked <lineked@yandex.ru>
2026-01-28 10:10:52 +0300

    Move Android-specific code and resources to new androidApp module

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    target {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    dependencies {
+        implementation(projects.kotfinance.composeApp)
+        implementation(libs.androidx.activity.compose)
+        implementation(libs.jetbrains.compose.ui.tooling.preview)
+        debugImplementation(libs.jetbrains.compose.ui.tooling)
+    }
+}
+
+android {
+    namespace = "com.cyberstarege.kotfinance"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.cyberstarege.kotfinance"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
