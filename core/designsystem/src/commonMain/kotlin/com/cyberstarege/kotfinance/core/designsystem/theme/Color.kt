ad8311b269f6aedfbbcab2ada3434bff6195ade8
tseked <lineked@yandex.ru>
2026-01-28 12:07:12 +0300

    Add colors

---
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.cyberstarege.kotfinance.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryDarkMode = Color(0xFF13A4EC)
+val OnPrimaryDarkMode = Color(0xFFFFFFFF)
+val SecondaryContainerDarkMode = Color(0xFF182830)
+val OnSecondaryContainerDarkMode = Color(0xFF94A3B8)
+
+val BackgroundDarkMode = Color(0xFF101C22)
+
+val SurfaceDarkMode = Color(0xFF182830)
+val SurfaceContainer = Color(0xFF64748B)
+val OnSurfaceDarkMode = Color(0xFFFFFFFF)
+val OnSurfaceVariantDarkMode = Color(0xFF94A3B8)
+
+val Green = Color(0xFF0BDA57)
+val Red = Color(0xFFEF4444)
+
+val OnBackgroundDarkMode = Color(0xFFFFFFFF)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
