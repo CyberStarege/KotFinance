8c068ca24b7f3e82b0112bd7509a38f1aa91f4d9
tseked <lineked@yandex.ru>
2026-01-28 10:13:49 +0300

    New dependencies

--- feature/groups/build.gradle.kts
+++ feature/groups/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.multiplatformLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
 }
 
 kotlin {
@@ -13,7 +16,16 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-
+            implementation(projects.core.designsystem)
+            implementation(projects.core.model)
+            implementation(libs.jetbrains.compose.runtime)
+            implementation(libs.jetbrains.compose.foundation)
+            implementation(libs.jetbrains.compose.material3)
+            implementation(libs.jetbrains.compose.ui)
+            implementation(libs.jetbrains.compose.components.resources)
+            implementation(libs.jetbrains.compose.ui.tooling.preview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
         }
     }
 }
