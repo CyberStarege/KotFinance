6d164a872af955626533d95b9c09c13cb3601704
tseked <lineked@yandex.ru>
2026-01-28 13:20:24 +0300

    add getAllAccountGroupsUseCase in constructor

--- feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
+++ feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
@@ -1,7 +1,10 @@
 package com.cyberstarege.kotfinance.feature.groups
 
 import androidx.lifecycle.ViewModel
+import com.cyberstarege.kotfinance.core.domain.GetAllAccountGroupsUseCase
 
-class GroupsViewModel : ViewModel() {
+class GroupsViewModel(
+    private val getAllAccountGroupsUseCase: GetAllAccountGroupsUseCase
+) : ViewModel() {
 
 }
