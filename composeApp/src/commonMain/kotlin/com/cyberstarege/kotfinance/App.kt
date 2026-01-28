064307d91a94e43f3814631eaa484396dfa9c0ef
tseked <lineked@yandex.ru>
2026-01-28 10:12:48 +0300

    update import

--- composeApp/src/commonMain/kotlin/com/cyberstarege/kotfinance/App.kt
+++ composeApp/src/commonMain/kotlin/com/cyberstarege/kotfinance/App.kt
@@ -13,8 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import kotfinance.composeapp.generated.resources.Res
 import kotfinance.composeapp.generated.resources.compose_multiplatform
