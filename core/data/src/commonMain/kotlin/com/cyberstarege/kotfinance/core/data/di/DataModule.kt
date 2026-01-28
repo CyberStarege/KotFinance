b214e43d62ca6e64318fa8d97527f5b698f45d98
tseked <lineked@yandex.ru>
2026-01-28 23:12:25 +0300

    Update dataModule to include platformModule

--- core/data/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/data/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/data/di/DataModule.kt
@@ -1,6 +1,7 @@
 package com.cyberstarege.kotfinance.core.data.di
 
 import com.cyberstarege.kotfinance.core.database.databaseModule
+import com.cyberstarege.kotfinance.core.database.platformModule
 import com.cyberstarege.kotfinance.core.data.repository.AccountGroupRepository
 import com.cyberstarege.kotfinance.core.data.repository.AccountGroupRepositoryImpl
 import org.koin.core.module.dsl.singleOf
@@ -8,6 +9,6 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-    includes(databaseModule)
+    includes(databaseModule, platformModule)
     singleOf(::AccountGroupRepositoryImpl) bind AccountGroupRepository::class
 }
