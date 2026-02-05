0ec6925333fc7a4016756b97a55f949070004be4
tseked <lineked@yandex.ru>
2026-02-05 13:53:25 +0300

    update module

--- core/data/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/data/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/data/di/DataModule.kt
@@ -4,6 +4,8 @@ import com.cyberstarege.kotfinance.core.database.databaseModule
 import com.cyberstarege.kotfinance.core.database.platformModule
 import com.cyberstarege.kotfinance.core.data.repository.AccountGroupRepository
 import com.cyberstarege.kotfinance.core.data.repository.AccountGroupRepositoryImpl
+import com.cyberstarege.kotfinance.core.data.repository.AccountRepository
+import com.cyberstarege.kotfinance.core.data.repository.AccountRepositoryImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -11,4 +13,5 @@ import org.koin.dsl.module
 val dataModule = module {
     includes(databaseModule, platformModule)
     singleOf(::AccountGroupRepositoryImpl) bind AccountGroupRepository::class
+    singleOf(::AccountRepositoryImpl) bind AccountRepository::class
 }
