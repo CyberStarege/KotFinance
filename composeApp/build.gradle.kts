fbad3df89b196ba34254fc3de209a1b6088ea062
tseked <lineked@yandex.ru>
2026-02-07 09:59:59 +0300

    renamed to composeApp

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 kotlin {
     androidLibrary {
-        namespace = "com.cyberstarege.kotfinance.shared"
+        namespace = "com.cyberstarege.kotfinance.composeApp"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
         androidResources {
@@ -36,8 +36,8 @@ kotlin {
             implementation(libs.jetbrains.compose.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
+            implementation(libs.kotlinx.coroutines.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
