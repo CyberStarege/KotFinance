73cf43e203cb3c2a61d508547b7c980549d58f0f
tseked <lineked@yandex.ru>
2026-02-08 08:55:05 +0300

    update icons

--- core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/KotFinanceIcons.kt
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/KotFinanceIcons.kt
@@ -1,9 +1,43 @@
 package com.cyberstarege.kotfinance.core.designsystem.icon
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
 import kotfinance.core.designsystem.generated.resources.Res
-import kotfinance.core.designsystem.generated.resources.ic_edit
-import kotfinance.core.designsystem.generated.resources.ic_add
+import kotfinance.core.designsystem.generated.resources.ic_account_balance_wallet_24px
+import kotfinance.core.designsystem.generated.resources.ic_edit_24px
+import kotfinance.core.designsystem.generated.resources.ic_add_24px
+import kotfinance.core.designsystem.generated.resources.ic_analytics_24px
+import kotfinance.core.designsystem.generated.resources.ic_home_24px
+import kotfinance.core.designsystem.generated.resources.ic_menu_24px
+import kotfinance.core.designsystem.generated.resources.ic_settings_24px
+import org.jetbrains.compose.resources.painterResource
+
 object KotFinanceIcons {
-    val Edit = Res.drawable.ic_edit
-    val Add = Res.drawable.ic_add
+    val Edit: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_edit_24px)
+
+    val Add: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_add_24px)
+
+    val Home: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_home_24px)
+
+    val AccountBalanceWallet: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_account_balance_wallet_24px)
+
+    val Settings: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_settings_24px)
+
+    val Menu: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_menu_24px)
+
+    val Analytics: Painter
+        @Composable
+        get() = painterResource(Res.drawable.ic_analytics_24px)
 }
