6e4d87de8f71cc3bd0f15e843f3ba133c70b61a9
tseked <lineked@yandex.ru>
2026-02-09 17:07:53 +0300

    add MobileLayout

---
+++ composeApp/src/commonMain/kotlin/com/cyberstarege/kotfinance/layouts/MobileLayout.kt
@@ -0,0 +1,32 @@
+package com.cyberstarege.kotfinance.layouts
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import com.cyberstarege.kotfinance.navigation.AppBottomBarNavigation
+import com.cyberstarege.kotfinance.navigation.AppNavigation
+
+@Composable
+fun MobileLayout(
+    navBackStack: NavBackStack<NavKey>,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            AppBottomBarNavigation(
+                currentKey = navBackStack.lastOrNull(),
+                onNavigate = { navKey -> navBackStack.add(navKey) }
+            )
+        }
+    ) { contentPadding ->
+        AppNavigation(
+            navBackStack = navBackStack,
+            modifier = Modifier.padding(contentPadding)
+        )
+    }
+
+}
