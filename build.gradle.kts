33e1911fae326246f5af36ef0280e54e127e3b5a
tseked <lineked@yandex.ru>
2026-02-09 17:07:30 +0300

    add navigation and serialization dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidxRoom) apply false
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
 }
