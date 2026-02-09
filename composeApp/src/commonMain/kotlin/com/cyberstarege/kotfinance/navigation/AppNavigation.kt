8554fc7fc0ff7d7e1548918bde710ecb1232c6bb
tseked <lineked@yandex.ru>
2026-02-09 17:07:01 +0300

    add AppNavigation

---
+++ composeApp/src/commonMain/kotlin/com/cyberstarege/kotfinance/navigation/AppNavigation.kt
@@ -0,0 +1,42 @@
+package com.cyberstarege.kotfinance.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
+import com.cyberstarege.kotfinance.feature.groups.GroupsScreen
+
+@Composable
+fun AppNavigation(
+    navBackStack: NavBackStack<NavKey>,
+    modifier: Modifier = Modifier
+) {
+    NavDisplay(
+        backStack = navBackStack,
+        onBack = { navBackStack.removeLastOrNull() },
+        entryProvider = entryProvider {
+            appEntry()
+        },
+        modifier = modifier
+    )
+}
+
+
+fun EntryProviderScope<NavKey>.appEntry() {
+    entry<Route.Home> {
+        Text("Главный экран")
+    }
+    entry<Route.Analytics> {
+        Text("Аналитика")
+    }
+    entry<Route.GroupsAccount> {
+        GroupsScreen()
+    }
+    entry<Route.Settings> {
+        Text("Настройки")
+    }
+}
