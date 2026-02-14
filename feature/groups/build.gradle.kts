60fba8b62b01b540bcfb9a4721117ce0aeb32299
tseked <lineked@yandex.ru>
2026-02-14 17:04:39 +0300

    add adaptive

--- feature/groups/build.gradle.kts
+++ feature/groups/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
             implementation(libs.jetbrains.compose.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.jetbrains.material3.adaptive)
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
