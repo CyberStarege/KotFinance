97acca9190cbef4effbd73fe6468bee60d538aae
tseked <lineked@yandex.ru>
2026-02-14 17:03:28 +0300

    add colors for select in group

---
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/KotFinanceColors.kt
@@ -0,0 +1,16 @@
+package com.cyberstarege.kotfinance.core.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+object KotFinanceColors {
+    val PresetColors = listOf(
+        Color(0xFF00BFFF),
+        Color(0xFF00E676),
+        Color(0xFFAA00FF),
+        Color(0xFFFF9100),
+        Color(0xFFFF1744),
+        Color(0xFF78909C),
+        Color(0xFFEC407A),
+        Color(0xFFFFEE58)
+    )
+}
