f5a915e791478d07783005a68a193ea22756ad60
tseked <lineked@yandex.ru>
2026-02-14 17:08:00 +0300

    update GroupsViewModel to use GroupsUiState and GetGroupsWithAccountsUseCase

--- feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
+++ feature/groups/src/commonMain/kotlin/com/cyberstarege/kotfinance/feature/groups/GroupsViewModel.kt
@@ -3,22 +3,34 @@ package com.cyberstarege.kotfinance.feature.groups
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cyberstarege.kotfinance.core.domain.AddGroupUseCase
+import com.cyberstarege.kotfinance.core.domain.GetAccountsByGroupIdUseCase
 import com.cyberstarege.kotfinance.core.domain.GetAllAccountGroupsUseCase
+import com.cyberstarege.kotfinance.core.domain.GetGroupsWithAccountsUseCase
 import com.cyberstarege.kotfinance.core.model.AccountGroup
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
+
 class GroupsViewModel(
     private val getAllAccountGroupsUseCase: GetAllAccountGroupsUseCase,
-    private val addGroupUseCase: AddGroupUseCase
+    private val getAccountsByGroupIdUseCase: GetAccountsByGroupIdUseCase,
+    private val addGroupUseCase: AddGroupUseCase,
+    private val getGroupsWithAccountsUseCase: GetGroupsWithAccountsUseCase,
 ) : ViewModel() {
-    val accountGroups: StateFlow<List<AccountGroup>> = getAllAccountGroupsUseCase()
+
+    val uiState: StateFlow<GroupsUiState> = getGroupsWithAccountsUseCase()
+        .map { groups ->
+            GroupsUiState(
+                groupsWithAccounts = groups
+            )
+        }
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.Lazily,
-            initialValue = emptyList()
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = GroupsUiState(groupsWithAccounts = emptyList())
         )
 
     fun addGroup(accountGroup: AccountGroup) {
