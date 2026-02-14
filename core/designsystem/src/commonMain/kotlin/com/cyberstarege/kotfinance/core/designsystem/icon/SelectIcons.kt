1a46145b7ec671b603448bb7d1f5e47667bd43cb
tseked <lineked@yandex.ru>
2026-02-14 17:07:36 +0300

    add SelectIcons

---
+++ core/designsystem/src/commonMain/kotlin/com/cyberstarege/kotfinance/core/designsystem/icon/SelectIcons.kt
@@ -0,0 +1,21 @@
+package com.cyberstarege.kotfinance.core.designsystem.icon
+
+import kotfinance.core.designsystem.generated.resources.Res
+import kotfinance.core.designsystem.generated.resources.*
+import org.jetbrains.compose.resources.DrawableResource
+
+enum class SelectIcons(val icon: DrawableResource) {
+    DINING(Res.drawable.ic_fork_spoon_24px),
+    FITNESS(Res.drawable.ic_fitness_center_24px),
+    WALLET(Res.drawable.ic_account_balance_wallet_24px),
+    CARD(Res.drawable.ic_credit_card_24px),
+    RUBLE(Res.drawable.ic_currency_ruble_24px),
+    BITCOIN(Res.drawable.ic_currency_bitcoin_24px),
+    MOBILE(Res.drawable.ic_mobile_24px),
+    EDUCATION(Res.drawable.ic_school_24px),
+    HOME(Res.drawable.ic_home_24px),
+    GROCERY(Res.drawable.ic_grocery_24px),
+    ANALYTICS(Res.drawable.ic_analytics_24px),
+    GIFT(Res.drawable.ic_redeem_24px),
+    CAR(Res.drawable.ic_directions_car_24px);
+}
